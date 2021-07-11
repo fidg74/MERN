fafb96109e92c3b244c107982e66e1a5848423d8
lipliepra <dzis99@yandex.ru>
2021-07-12 00:05:07 +0500

    create route 'Registration'

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+
+const server = express()
+
+const authRoute = require('./routes/auth.route')
+// applicationCache.use('/api/auth', authRoute)
+
+const PORT = config.get('port') || 5000
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoURL'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+        server.listen(PORT, () => console.log('Server has been started on port ' + PORT))
+    } catch (e) {
+        console.log('Server Error', e.message)
+        process.exit(1)
+    }
+}
+
+start()
+
