327ef44c022f9bc1c5f62f2cc8959b8036f22ad2
lipliepra <dzis99@yandex.ru>
2021-07-19 21:01:32 +0500

    add updateTextFields

--- client/src/pages/AuthPage/AuthPage.js
+++ client/src/pages/AuthPage/AuthPage.js
@@ -1,15 +1,15 @@
-import React, {useEffect, useContext, useState} from 'react'
+import React, { useEffect, useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 import './AuthPage.css'
 import 'materialize-css'
-import {useHttp} from '../../hooks/http.hook'
-import {useMessage} from '../../hooks/message.hook'
+import { useHttp } from '../../hooks/http.hook'
+import { useMessage } from '../../hooks/message.hook'
 import { AuthContext } from '../../context/AuthContext'
 
 const AuthPage = () => {
     const auth = useContext(AuthContext)
     const message = useMessage()
-    const {loading, request, error, clearError} = useHttp()
+    const { loading, request, error, clearError } = useHttp()
     const [form, setForm] = useState({
         login: '', email: '', password: ''
     })
@@ -19,16 +19,20 @@ const AuthPage = () => {
         clearError()
     }, [error, message])
 
+    useEffect(() => {
+        window.M.updateTextFields()
+    }, [])
+
     const changeHandler = event => {
-        setForm({...form, [event.target.name]: event.target.value})
+        setForm({ ...form, [event.target.name]: event.target.value })
     }
 
     const loginHandler = async () => {
         try {
-          const data = await request('/api/auth/login', 'POST', {...form})
-          auth.login(data.token, data.userID)
-        } catch (e) {}
-      }
+            const data = await request('/api/auth/login', 'POST', { ...form })
+            auth.login(data.token, data.userID)
+        } catch (e) { }
+    }
 
     return (
         <div className="container">
@@ -38,21 +42,21 @@ const AuthPage = () => {
                     <div className="card-content white-text">
                         <span className="card-title">Авторизация</span>
                         <div>
-                        <div className="input-field">
+                            <div className="input-field">
                             </div>
                             <div className="input-field">
-                            <input 
-                                   id="login"
-                                   type="text"
-                                   name="login"
-                                   className="validate white-text"
-                                   onChange={changeHandler} />
+                                <input
+                                    id="login"
+                                    type="text"
+                                    name="login"
+                                    className="validate white-text"
+                                    onChange={changeHandler} />
                                 <label htmlFor="login" className="white-text">Введите login</label>
                             </div>
                             <div className="input-field">
-                                <input 
+                                <input
                                     id="password"
-                                    type="password" 
+                                    type="password"
                                     name="password"
                                     className="validate white-text"
                                     onChange={changeHandler} />
@@ -62,26 +66,26 @@ const AuthPage = () => {
                     </div>
                     <div className="card-action">
                         <button
-                            
-                            className="btn yellow darken-3" 
-                            style={{marginRight: 40}} 
+
+                            className="btn yellow darken-3"
+                            style={{ marginRight: 40 }}
                             disabled={loading}
                             onClick={loginHandler}
                         >
                             Войти
                         </button>
                         <Link to="/">
-                    <button 
-                        className="btn orange darken-2"
-                    >
-                        Назад
-                    </button>
-                    </Link>
+                            <button
+                                className="btn orange darken-2"
+                            >
+                                Назад
+                            </button>
+                        </Link>
                     </div>
                 </div>
-                
+
             </div>
-            
+
         </div>
     )
 }
